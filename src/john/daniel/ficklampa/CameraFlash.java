5284d3535de1d6237e4eefd134a2909a3b38f10f
John Daniel <daniel@bosser.com>
2013-02-03 14:22:12 +0100

    add button and new activity to launch camera flash

---
+++ src/john/daniel/ficklampa/CameraFlash.java
@@ -0,0 +1,15 @@
+package john.daniel.ficklampa;
+
+import android.app.Activity;
+import android.view.Menu;
+
+public class CameraFlash extends Activity{
+
+	@Override
+	public void onContextMenuClosed(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onContextMenuClosed(menu);
+		setContentView(R.layout.);
+	}
+	
+}
--- src/john/daniel/ficklampa/MainActivity.java
+++ src/john/daniel/ficklampa/MainActivity.java
@@ -1,30 +1,52 @@
 package john.daniel.ficklampa;
 
+import john.daniel.lek.MainActivity;
+import john.daniel.lek.NewScreen;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private SeekBar bar;
-	private TextView textField;
-	float screenBrightness;
+	SeekBar bar;
+	TextView textField;
+	Button launchFlash;
+	float setBrightness;
 	float value;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        //Set up button to launch new activity to drive the Flash
+        launchFlash = (Button) findViewById(R.id.launchFlashButton);
+        launchFlash.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, CameraFlash.class); 
+				startActivity(intent);
+			}
+		});
+        
+        //Setting up seekbar
         bar = (SeekBar) findViewById(R.id.screenValue);
         bar.setProgress(0);
         bar.setMax(100);
         bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
         	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        		screenBrightness = (float)progress / 100;
-        		value = screenBrightness * 100;
+        		//screenBrightness has float value between 0 and 1. Progress/100 will make a 0.x value to screenBrightness
+        		setBrightness = (float)progress / 100;
+        		value = setBrightness * 100;
         		WindowManager.LayoutParams layout = getWindow().getAttributes();
-        		layout.screenBrightness = screenBrightness;
+        		layout.screenBrightness = setBrightness;
         		getWindow().setAttributes(layout);
         		
         		textField = (TextView) findViewById(R.id.textField);
@@ -43,5 +65,6 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated method stub
 			};
     });
+        
 }
 }
